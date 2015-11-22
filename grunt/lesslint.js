6ab2fe218049ca8ed10cb949cd1ec99ff6b06c93
Amr Abdulrahman <abu.muslim.amr@gmail.com>
2015-11-22 16:01:34 +0200

    Add less linter to grunt tasks

---
+++ grunt/lesslint.js
@@ -0,0 +1,7 @@
+module.exports = function(grunt, config) {
+  return {
+    src: [
+        config.styles.src
+    ]
+  };
+};
--- grunt/watch.js
+++ grunt/watch.js
@@ -14,6 +14,7 @@ module.exports = function(grunt, config) {
     styles: {
       files: config.styles.src,
       tasks: [
+        'lesslint',
         'less'
       ],
       options: {
